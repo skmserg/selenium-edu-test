d65c244e12de53d80be2b50b7c892bf6e851790a
skmserg <31239341+skmserg@users.noreply.github.com>
2019-03-08 13:51:58 +0300

    Revert "удален импорт не используемых библиотек"
    This reverts commit 7e3d052ae247e0dbca4f563088d569966bb5627e.

--- src/test/java/ru/sskm/FirstTest.java
+++ src/test/java/ru/sskm/FirstTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class FirstTest {
 
