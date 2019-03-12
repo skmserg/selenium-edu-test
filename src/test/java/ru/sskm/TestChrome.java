c6b753def252f7532eb5122f7d60fb311fb0a36d
skmserg <31239341+skmserg@users.noreply.github.com>
2019-03-13 00:18:39 +0300

    add_HW4_1

--- src/test/java/ru/sskm/TestChrome.java
+++ src/test/java/ru/sskm/TestChrome.java
@@ -3,11 +3,19 @@ package ru.sskm;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 public class TestChrome {
@@ -18,16 +26,37 @@ public class TestChrome {
     @Before
     public void start(){
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void myFirstTest(){
+    public void myFirstTestChrome() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+
+        List<WebElement> listLiApp = driver.findElements(By.cssSelector("li#app-"));
+        for (int i = 1; i < listLiApp.size() + 1; i++) {
+            driver.findElement(By.cssSelector("li#app-:nth-child(" + i + ")")).click();
+            assertTrue(isPresent(By.cssSelector("h1")));
+
+            List<WebElement> listLiDocs = driver.findElements(By.cssSelector("ul.docs li"));
+            for (int j = 1; j < listLiDocs.size() + 1; j++) {
+                driver.findElement(By.cssSelector("ul.docs li:nth-child(" + j + ")")).click();
+                assertTrue(isPresent(By.cssSelector("h1")));
+            }
+        }
+    }
+
+    public boolean isPresent(By locator){
+        try{
+            driver.findElement(locator);
+            return true;
+        }catch (NoSuchElementException ex){
+            return false;
+        }
     }
 
     @After
@@ -35,5 +64,7 @@ public class TestChrome {
         driver.quit();
         driver = null;
     }
+
+
 
 }
